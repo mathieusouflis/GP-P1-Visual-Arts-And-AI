7301faeb9a5c8b460e3736c13eb9fd1149a8e7db
Mathieu <mathieu@souflis.fr>
2024-11-07 14:38:25 +0100

    Router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ReactLenis } from "lenis/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
-import App from "./App.jsx";
+import Homepage from "./Pages/homepage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ReactLenis root>
-      <App />
+      <RouterProvider router={router} />
     </ReactLenis>
   </StrictMode>,
 );
